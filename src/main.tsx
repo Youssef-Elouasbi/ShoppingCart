033ec8d1e4913996faedecfd54152653f05a0147
Youssef Elouasbi <youssef.ws20199@gmail.com>
2023-03-01 18:28:33 +0000

    change to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, HashRouter as HRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <HRouter>
       <App />
-    </Router>
+    </HRouter>
   </React.StrictMode>,
 )
